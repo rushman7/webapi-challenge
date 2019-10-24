1a90d023fdfb34b0eef913d14da1ee24eccac7c0
BuildTools <arthurpisakhov1@gmail.com>
2019-10-23 22:23:05 -0400

    setup Projects displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      New App
+      <h2>Projects and Actions</h2>
+      <Projects />
     </div>
   );
 }
---
+++ client/src/components/Projects.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Dimmer, Loader, Button } from 'semantic-ui-react'
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Projects = () => {
+  const [projects, setProjects] = useState();
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('http://localhost:5000/api/projects')
+      .then(res => {
+        console.log(res.data)
+        setProjects(res.data)
+      })
+      .catch(err => console.log(err));
+  }, [])
+
+  if (!projects) {
+    return   (
+      <div>
+        <Dimmer active>
+          <Loader>Loading...</Loader>
+        </Dimmer>
+      </div>
+    )
+  }
+  return (
+    <div className="project-cont">
+      {projects.map(project => 
+        <div className="projects" key={project.id}>
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+          <Button className="button-projects" secondary>Details</Button>
+        </div>)}
+    </div>
+  );
+}
+
+export default Projects;
