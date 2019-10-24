2814618f4ae1f12fb46d7e77059f0dea3a23b9cb
BuildTools <arthurpisakhov1@gmail.com>
2019-10-23 21:11:51 -0400

    GET and GET by ID

---
+++ routes/projectRouter.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const projectDB = require('../data/helpers/projectModel');
+const actionDB = require('../data/helpers/actionModel');
+
+const router = express.Router();
+
+// router.post('/', validateUser, (req, res) => {
+//   userDB.insert(req.body)
+//     .then(user => res.status(201).json(user))
+//     .catch(() => res.status(500).json({ error: "There was an error while saving the user to the database" }))
+// });
+
+// router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+//   postDB.insert(req.body)
+//     .then(post => res.status(201).json(post))
+//     .catch(() => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+// });
+
+router.get('/', (req, res) => {
+  const sortField = req.query.sortBy || 'id';
+
+  projectDB.get()
+    .then(projects => {
+      const response = projects.sort((a,b) => a[sortField] < b[sortField] ? -1 : 1)
+      res.status(200).json(response)
+    })
+    .catch(() => res.status(500).json({ error: "The projects information could not be retrieved." }))
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(200).json(req.project)
+});
+
+// router.get('/:id/posts', validateUserId, (req, res) => {
+//   userDB.getUserPosts(req.user.id)
+//     .then(posts => {
+//       if (posts.length > 0) res.status(200).json(posts)
+//       else res.status(404).json({ error: "The posts with the specified user ID does not exist." })
+//     })
+//     .catch(() => res.status(500).json({ error: "The posts information could not be retrieved." }))
+// });
+
+// router.delete('/:id', validateUserId, (req, res) => {
+//   userDB.remove(req.user.id)
+//     .then(user => {
+//       if (user) res.status(202).json({ error: `The user with the ID ${req.user.id} has been removed.` })
+//       else res.status(404).json({ error: "The user with the specified ID does not exist." })
+//     })
+//     .catch(() => res.status(500).json({ error: "The user could not be removed" }))
+// });
+
+// router.put('/:id', validateUserId, validateUser, (req, res) => {
+//   userDB.update(req.user.id, req.body)
+//     .then(user => {
+//       if (user) res.status(200).json({ error: `The user with the ID ${req.user.id} has been updated.` })
+//       else res.status(404).json({ error: "The user with the specified ID does not exist." })
+//     })
+//     .catch(() => res.status(500).json({ error: "The user information could not be modified." }))
+// });
+
+// custom middleware
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id;
+
+  projectDB.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else res.status(404).json({ message: "invalid project id" })
+    })
+    .catch(() => res.status(500).json({ error: "The projects information could not be retrieved." }))
+};
+
+// function validateUser(req, res, next) {
+//   const { name } = req.body;
+
+//   if (!req.body) res.status(400).json({ message: "missing user data" })
+//   else if (!name) res.status(400).json({ message: "missing required name field" })
+//   else next();
+// };
+
+// function validatePost(req, res, next) {
+//   const { text } = req.body;
+
+//   if (!req.body) res.status(400).json({ message: "missing post data" })
+//   else if (!text) res.status(404).json({ message: "missing required text field" })
+//   else next();
+// };
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const projectRouter = require('./routes/projectRouter');
+
+require('dotenv').config();
+
+const server = express();
+const port = process.env.PORT;
+
+server.use(express.json())
+server.use(cors());
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
+};
+
+server.use(logger);
+
+server.use((err, req, rest, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'There was an error performing the required operation', error: err })
+})
+
+server.use('/api/projects', projectRouter)
+
+server.listen(port, () => console.log(`\n*** Server Running on http://localhost:${port} ***\n`));
